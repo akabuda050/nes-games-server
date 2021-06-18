4753c21e72c992cb6177a89b0d85cd8f514129f1
Olexandr Dolbin <olexandr.dolbin@gmail.com>
2021-06-18 03:20:03 +0300

    improve validation

--- src/rooms/controllers/rooms.controller.ts
+++ src/rooms/controllers/rooms.controller.ts
@@ -4,7 +4,9 @@ import {
   Controller,
   Delete,
   Get,
+  HttpStatus,
   Param,
+  ParseUUIDPipe,
   Post,
   Put,
   UsePipes,
@@ -14,6 +16,9 @@ import { CreateRoomDto } from '../dto/create-room.dto';
 import { UpdateRoomDto } from '../dto/update-room.dto';
 import { Room } from '../interfaces/room.interface';
 import { RoomsService } from '../services/rooms.service';
+const UUIDPipe = new ParseUUIDPipe({
+  errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE,
+});
 
 @Controller('rooms')
 export class RoomsController {
@@ -25,7 +30,10 @@ export class RoomsController {
   }
 
   @Get(':id')
-  async findById(@Param('id') id: string): Promise<Room> {
+  async findById(
+    @Param('id', UUIDPipe)
+    id: string,
+  ): Promise<Room> {
     return this.roomsService.findById(id);
   }
 
@@ -37,12 +45,19 @@ export class RoomsController {
 
   @Put(':id')
   @UsePipes(new ValidationPipe({ transform: true }))
-  async update(@Param('id') id: string, @Body() updateRoomDto: UpdateRoomDto) {
+  async update(
+    @Param('id', UUIDPipe)
+    id: string,
+    @Body() updateRoomDto: UpdateRoomDto,
+  ) {
     this.roomsService.update(id, updateRoomDto);
   }
 
   @Delete(':id')
-  async delete(@Param('id') id: string) {
+  async delete(
+    @Param('id', UUIDPipe)
+    id: string,
+  ) {
     return this.roomsService.delete(id);
   }
 }
