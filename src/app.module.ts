46f4649cb4193306e5fa8b128e81f98f91c823e9
Olexandr Dolbin <olexandr.dolbin@gmail.com>
2021-06-24 21:39:00 +0300

    add database support

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,25 @@
+import * as Joi from '@hapi/joi';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { RoomsModule } from '@src/rooms/rooms.module';
 import { UsersModule } from '@src/users/users.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [UsersModule, RoomsModule],
+  imports: [
+    UsersModule,
+    RoomsModule,
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        PORT: Joi.number(),
+      }),
+    }),
+    DatabaseModule
+  ],
 })
 export class AppModule {}
